98c416355b3470caccf1bc35e7fca1ff3c4edb7f
2Emma-zhou <110969310+2Emma-zhou@users.noreply.github.com>
2026-06-10 17:05:45 +0800

    Add GTM and update navbar official link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,36 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
+const GTM_ID = 'GTM-NRRWS67W';
+
+function googleTagManagerPlugin() {
+  return {
+    name: 'google-tag-manager-plugin',
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: 'script',
+            innerHTML: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          },
+        ],
+        preBodyTags: [
+          {
+            tagName: 'noscript',
+            innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          },
+        ],
+      };
+    },
+  };
+}
+
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
@@ -86,6 +116,7 @@ const config = {
   // themes: ['@docusaurus/theme-search-algolia'],
   plugins: [
     require.resolve("plugin-image-zoom"),  //zoom images in document
+    googleTagManagerPlugin,
   ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -116,7 +147,7 @@ const config = {
         logo: {
           alt: 'Tier0',
           src: 'img/black logo.svg',
-          href: '/introduction',
+          href: 'https://tier0.app',
           srcDark: 'img/white logo.svg',
         },
         items: [
@@ -138,11 +169,6 @@ const config = {
           type: 'localeDropdown',
           position: 'right',
         },
-          {
-            href: 'https://tier0.app/',
-            label: 'Official Site',
-            position: 'right',
-          },
         ],
       },
       footer: {
